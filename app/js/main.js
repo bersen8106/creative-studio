23b173725df286754373b8becfa9ace8354acea3
bersen8106 <bersen8106@gmail.com>
2020-10-25 18:20:39 +0500

    added header and experience sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function() {
+    $('.header__menu-btn').on('click', function() {
+        $('.header__menu-list').toggleClass('active');
+    });
 
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 1) {
+    //         $('.header__top').addClass('scroll');
+    //     } else {
+    //         $('.header__top').removeClass('scroll');
+    //     }
+    // });
 });
