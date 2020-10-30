e1ab91c2fca27f2210f20568de33b4b4f2070983
bersen8106 <bersen8106@gmail.com>
2020-10-30 18:44:29 +0500

    added portfolio history prices and team sections

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,45 @@ $(function() {
     //         $('.header__top').removeClass('scroll');
     //     }
     // });
+    //------------------------------------
+    $('.portfolio__btn').on('focus', function() {
+        $(this).parents('.portfolio-corner').removeClass('hide');
+        //поиск родителей элемента '.portfolio__btn' у которых есть класс
+    }); //'.portfolio-corner'
+
+    $('.portfolio__btn').on('focusout', function() {
+        $(this).parents('.portfolio-corner').addClass('hide');
+        //поиск родителей элемента '.portfolio__btn' у которых есть класс
+    }); //'.portfolio-corner'
+    //------------------------------------
+
+
+    // $('.portfolio__btn.mixitup-control-active').on('click', function() {
+    //     $(this).parents('.portfolio-corner').removeClass('hide');
+
+    //     //поиск родителей элемента '.portfolio__btn' у которых есть класс
+    // }); //'.portfolio-corner'
+
+
+
+    // $('.portfolio__btn').on('click', function() {
+    //     $(this).parents('.portfolio-corner').removeClass('hide');
+    //     $(this).parents('.portfolio-corner').addClass('hide');
+
+
+
+
+
+    // $('.btn').hover(function() {
+    //     $('.top-corner').toggleClass('hide');
+    // });
+    // $('.btn').hover(function() {
+    //     $('.bottom-corner').toggleClass('hide');
+    // });
+    // $('.btn').hover(function() {
+    //     $('.top-corner').toggleClass('hide');
+    // });
+
+    var mixer = mixitup('.portfolio__images');
+
 });
